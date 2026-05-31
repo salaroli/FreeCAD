fab8345b10b871e14bd33c7d484fe3c84775adb7
Salaroli <fernando@salaro.li>
2026-05-31 17:14:32 -0300

    fix: use v1-1/Mod/ path for IconThemes addon (FreeCAD 1.1)

--- apply.sh
+++ apply.sh
@@ -3,11 +3,12 @@ set -e
 
 REPO="$(dirname "$0")"
 FREECAD_DATA="${HOME}/.local/share/FreeCAD"
+FREECAD_VER="${FREECAD_DATA}/v1-1"
 
-# IconThemes addon
-mkdir -p "${FREECAD_DATA}/Mod"
-rm -rf "${FREECAD_DATA}/Mod/IconThemes"
-cp -r "${REPO}/IconThemes" "${FREECAD_DATA}/Mod/IconThemes"
+# IconThemes addon (FreeCAD 1.1 usa v1-1/Mod/)
+mkdir -p "${FREECAD_VER}/Mod"
+rm -rf "${FREECAD_VER}/Mod/IconThemes"
+cp -r "${REPO}/IconThemes" "${FREECAD_VER}/Mod/IconThemes"
 echo "applied: IconThemes addon"
 
 # Flat_Dark icon theme
